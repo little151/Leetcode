35722ad2e22b56b7bbd43e65bcff048324268e2f
Divyansh Gautam <141272684+little151@users.noreply.github.com>
2024-10-19 08:34:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string s) {
+        // code here
+        transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+            return tolower(c);
+        });
+        return s;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.toLower(s) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
